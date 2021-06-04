6360a6c9a8c7cdc8ed0e4a7c4f94286f2d530098
zkw123 <906269996@qq.com>
2021-06-04 23:14:47 +0800

    commit

---
+++ IK/Assets/IK/Scripts/RobotMove.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    public Transform[] FootTarget;
+    public Transform[] Pole;
+    public Transform Robot;
+    public float Movespeed = 1.0f;
+    public float Runspeed = 3.0f;
+    public float RotateSpeed = 180f;
+    private bool move = false;
+    private bool run = false;
+    public Animator anim;
+    float originy;
+    Vector3 origin;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        originy= FootTarget[0].position.y;
+        origin = Robot.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        
+        for (int i = 0; i < FootTarget.Length; i++)
+        {
+            var foot = FootTarget[i];
+            var pole = Pole[i];
+            var ray = new Ray(foot.transform.position + Vector3.up * 1f, Vector3.down);
+            var hitInfo = new RaycastHit();
+            if (Physics.SphereCast(ray, 0.05f, out hitInfo, 2f))
+            {
+                //foot.position = hitInfo.point + Vector3.up * 0.05f;
+                Vector3 position = foot.position;
+                if (hitInfo.transform.name != "Ground")
+                {
+                   
+                    position.y = hitInfo.point.y;
+                    foot.position = position + Vector3.up * 0.05f;
+                     
+                   
+                }
+                else
+                {
+                    Robot.position = new Vector3(Robot.position.x, origin.y, Robot.position.z); ;
+                }
+                position = pole.position;
+                position.y = foot.position.y + 1f;
+                pole.position = position;
+            }
+            
+        }
+        float average=100;
+        for (int i = 0; i < FootTarget.Length; i++)
+        {
+            var foot = FootTarget[i];
+            if (foot.transform.position.y - originy < average)
+            {
+                average = foot.transform.position.y - originy;
+            }
+           
+            
+        }
+        Robot.position = new Vector3(Robot.position.x, origin.y + average, Robot.position.z);
+
+    }
+    private void Update()
+    {
+        move = false;
+        run = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            move = true;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                
+                    run = true;
+                
+            }
+            if (run)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * Runspeed, Space.Self);
+            }
+            else
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * Movespeed, Space.Self);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            move = true;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+
+                run = true;
+
+            }
+            if (run)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * Runspeed, Space.Self);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * Movespeed, Space.Self);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x = 0;
+            eulerAngles.y = eulerAngles.y - RotateSpeed * Time.deltaTime;
+            eulerAngles.z = 0;
+            transform.eulerAngles = eulerAngles;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x = 0;
+            eulerAngles.y = eulerAngles.y + RotateSpeed * Time.deltaTime;
+            eulerAngles.z = 0;
+            transform.eulerAngles = eulerAngles;
+
+        }
+        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            anim.SetTrigger("jump");
+        }
+        if (!move)
+        {
+            anim.SetBool("Moving", false);
+        }
+        else
+        {
+            anim.SetBool("Moving", true);
+        }
+        if(run)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+    }
+
+       
+        
+   
+}
---
+++ IK/Assets/QuadrupedRobotPack/Scenes/Example/Scripts/demo_animator.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class demo_animator : MonoBehaviour {
+
+
+    public Slider slider;
+    public Text infoText;
+    public GameObject asset;
+    private Animator anim;
+
+    private bool gunBarrelSpinning = false;
+
+    public Material[] mats;
+
+    private int currentTexIndex = 0;
+
+    void Start () {
+       
+        anim = asset.GetComponent<Animator>();
+        infoText.text = "";
+    }	
+	public void Walk1()
+    {
+        anim.SetTrigger("walk1");
+        infoText.text = "Walk";
+    }
+    public void Run()
+    {
+        anim.SetTrigger("run");
+        infoText.text = "Run";
+    }
+    public void Walk2()
+    {
+        anim.SetTrigger("walk2");
+        infoText.text = "Walk 2";
+    }
+    public void Walk3()
+    {
+        anim.SetTrigger("walk3");
+        infoText.text = "Walk 3";
+    }
+    public void Idle()
+    {
+        anim.SetTrigger("idle");
+        infoText.text = "Idle";
+    }
+    public void Sit()
+    {
+        anim.SetTrigger("sit");
+        infoText.text = "Sit/Crouch";
+    }
+
+    public void Jump()
+    {
+        anim.SetTrigger("jump");
+        infoText.text = "Jump";
+    }
+    public void Stand()
+    {
+        anim.SetTrigger("stand");
+        infoText.text = "Stand";
+    }
+
+    public void SpeedSliderChange()
+    {
+        anim.speed = slider.value;
+    }
+
+    
+    public void Die()
+    {
+        anim.SetTrigger("die");
+        infoText.text = "die";
+    }
+
+    public void NextTexture()
+    {
+        currentTexIndex++;
+
+        if(currentTexIndex >= mats.Length)
+        {
+            currentTexIndex = 0;
+        }
+      
+        Renderer rend = asset.GetComponentInChildren<Renderer>();
+        Debug.Log("hi");
+        rend.material = mats[currentTexIndex];
+        infoText.text = rend.material.name;
+    }
+
+    public void ToggleGunBarrel()
+    {
+        if (gunBarrelSpinning)
+        {
+            anim.SetTrigger("stopGunBarrelSpin");
+            gunBarrelSpinning = false;
+        }
+        else
+        {
+            anim.SetTrigger("gunBarrelSpin");
+            gunBarrelSpinning = true;
+        }
+        
+
+    }
+
+    //public void TogglePeripheral(string name)
+    //{
+
+    //    GameObject go = transform.Find(name).gameObject;
+
+    //    MeshRenderer mr = go.GetComponent<MeshRenderer>();
+
+
+    //    mr.enabled = !mr.enabled;
+
+    //    if(name == "cap_right")
+    //    {
+    //        TogglePeripheral("cap_left");
+    //    }
+    //}
+
+
+    //public void TogglePeripheral(string name)
+    //{
+
+    //    switch(name)
+    //    {
+    //        case "P1":
+    //            Renderer rend1 = p1.GetComponent<Renderer>();
+    //            rend1.enabled = !rend1.enabled;
+    //            break;
+    //        case "P2":
+    //            Renderer rend2 = p2.GetComponent<Renderer>();
+    //            rend2.enabled = !rend2.enabled;
+    //            break;
+    //        case "P3":
+    //            Renderer rend3 = p3.GetComponent<Renderer>();
+    //            rend3.enabled = !rend3.enabled;
+    //            break;
+    //        case "P4":
+    //            Renderer rend4 = p4.GetComponent<Renderer>();
+    //            rend4.enabled = !rend4.enabled;
+    //            break;
+    //        case "P5":
+    //            Renderer rend5 = p5.GetComponent<Renderer>();
+    //            rend5.enabled = !rend5.enabled;
+    //            break;
+    //    }
+    //}
+
+    //public void RandomiseColors()
+    //{
+    //    Color _primary = new Color(Random.value, Random.value, Random.value, 1.0f);
+    //    Color _secondary = new Color(Random.value, Random.value, Random.value, 1.0f);
+    //    Color _details = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+    //    primary.color = _primary;
+    //    secondary.color = _secondary;
+    //    details.color = _details;
+    //}
+
+    //public void ToggleTextured()
+    //{
+    //    Renderer rend = Textured.GetComponentInChildren<Renderer>();
+    //    if (rend.enabled == true)
+    //    {
+    //        transform.position = new Vector3(0, 0, 0);
+    //        Textured.GetComponentInChildren<Renderer>().enabled = false;
+    //    }
+    //    else
+    //    {
+    //        transform.position = leftPos.transform.position;
+    //        Textured.GetComponentInChildren<Renderer>().enabled = true;
+    //    }
+
+    //}
+}
